3120010a5aa5c91bfc9d31ce191c8fae8d9c29e6
Rodrigo de Salvidea <rodesalvideal@gmail.com>
2024-09-22 17:09:55 -0400

    add various changes

--- ethan.c
+++ ethan.c
@@ -21,10 +21,31 @@ bool is_check(struct Board *board){
 bool is_stalemate(struct Board *board){
 
 }
-int attacks(struct Board *board){
+int attacks(struct Board *board, Position *position){
+  char piece = board -> board[position->x][position->y];
+  case WHITE KING:
+  case WHITE QUEEN:
+  case WHITE PAWN:
+  case WHITE KNIGHT:
+  case WHITE BISHOP:
+  case WHITE ROOK:
 
+  case BLACK KING:
+  case BLACK QUEEN:
+  case BLACK PAWN:
+  case BLACK KNIGHT:
+  case BLACK BISHOP:
+  case BLACK ROOK;
 }
-
+struct Board*  makeBoard(){
+    struct Board *board = (struct Board*) malloc(sizeof(struct Board));
+    for (int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            board ->board[i][j] = 0;
+        }
+    }
+    return board;
+}
 
 
 
--- ethan.h
+++ ethan.h
@@ -1,14 +1,22 @@
 #include <stdio.h>
-
+#define BLACK -1 *
+#define WHITE -1 *
 #define KING 1
 #define QUEEN 2
 #define ROOK 3
 #define KNIGHT 4
 #define BISHOP 5
 #define PAWN 6
+#define ISWHITE > 0
+#define ISBLACK < 0
 
 struct Board{
   char[8][8] board;
+}
+
+struct Position{
+  unsigned char x;
+  unsigned char y;
 }
 
 char *get_moves(struct Board *board, int piece);
@@ -18,6 +26,7 @@ int evaluate_move(struct Board *board, int piece);
 bool is_mate(struct Board *board);
 bool is_check(struct Board *board);
 bool is_stalemate(struct Board *board);
+struct Board* makeBoard();
 
 
 
